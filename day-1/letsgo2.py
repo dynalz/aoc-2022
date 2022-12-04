754f37ebf94147b0d7ddde3658704ff86231b872
Lkxolp <tonyramos237@hotmail.com>
2022-12-04 00:21:57 +0000

    :sparkles: day1

--- day-1/letsgo2.py
+++ day-1/letsgo2.py
@@ -1,14 +1,15 @@
 with open("input.txt", "r") as f:
     text = f.read()
 
-max_calories = 0
+max_calories = [0,0,0]
 current_calories = 0
 for line in text.split("\n"):
     if not line:
-        if current_calories > max_calories:
-            max_calories = current_calories
+        if current_calories > min(max_calories):
+            max_calories[max_calories.index(min(max_calories))] = current_calories
         current_calories = 0
         continue
     current_calories += int(line)
 
 print(max_calories)
+print(sum(max_calories))
